49aecc54303f2dc5dd539f563934a94d2209a376
KathiaBernal <kathiabernal@gmail.com>
2022-10-12 22:25:13 -0600

    Algunos ejercicios del módulo 2

---
+++ 2.1.1.20 Dando formato a la salida.py
@@ -0,0 +1,54 @@
+#Intenta:
+"""
+Minimizar el número de invocaciones de la función print() insertando la secuencia \n en las cadenas.
+Hacer la flecha dos veces más grande (pero mantener las proporciones).
+Duplicar la flecha, colocando ambas flechas lado a lado; nota: una cadena se puede multiplicar usando el siguiente truco: "cadena" * 2 producirá "cadenacadena" (te contaremos más sobre ello pronto).
+"""
+
+print("    *")
+print("   * *")
+print("  *   *")
+print(" *     *")
+print("*       *")
+print("****  ****")
+print("  *   *")
+print("  *   *")
+print("  *   *")
+print("  *****")
+
+print("    *\n   * *\n  *   *\n *     *\n*       *\n****  ****\n  *   *\n  *   *\n  *   *\n  *****")
+
+print("        *")
+print("       * *")
+print("      *   *")
+print("     *     *")
+print("    *       *")
+print("   *         *")
+print("  *           *")
+print(" *             *")
+print("*****       *****")
+print("    *       *")
+print("    *       *")
+print("    *       *")
+print("    *       *")
+print("    *       *")
+print("    *********")
+
+print("        *\n       * *\n      *   *\n     *     *\n    *       *\n   *         *\n  *           *\n *             *\n*****       *****\n    *       *\n    *       *\n    *       *\n    *       *\n    *       *\n    *********")
+
+
+print("        *        "*2)
+print("       * *       "*2)
+print("      *   *      "*2)
+print("     *     *     "*2)
+print("    *       *    "*2)
+print("   *         *   "*2)
+print("  *           *  "*2)
+print(" *             * "*2)
+print("*****       *****"*2)
+print("    *       *    "*2)
+print("    *       *    "*2)
+print("    *       *    "*2)
+print("    *       *    "*2)
+print("    *       *    "*2)
+print("    *********    "*2)
---
+++ 2.1.1.6 La funcion print().py
@@ -0,0 +1,7 @@
+"""
+Utiliza la función print() para imprimir la linea "¡Hola, Mundo!" en la pantalla.
+Una vez hecho esto, utiliza la función print() nuevamente, pero esta vez imprime tu nombre.
+"""
+
+print("Hola Mundo")
+print("Kathia Bernal")
