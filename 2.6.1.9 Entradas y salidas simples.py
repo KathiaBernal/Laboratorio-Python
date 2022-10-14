9ceb7a906f4b4ab88b399512a2860f7db8649ecd
KathiaBernal <kathiabernal@gmail.com>
2022-10-14 17:32:11 -0600

    Se agregan más ejercicios de práctica del módulo 2

--- 2.6.1.9 Entradas y salidas simples.py
+++ 2.6.1.9 Entradas y salidas simples.py
@@ -1,4 +1,5 @@
-"""La tarea es completar el código para evaluar y mostrar el resultado de cuatro operaciones aritméticas básicas.
+"""
+La tarea es completar el código para evaluar y mostrar el resultado de cuatro operaciones aritméticas básicas.
 
 El resultado debe ser mostrado en consola.
 
@@ -6,4 +7,25 @@ Quizá no podrás proteger el código de un usuario que intente dividir entre ce
 
 Prueba tu código - ¿Produce los resultados esperados?
 
-No te mostraremos ningún dato de prueba, eso sería demasiado sencillo."""
+No te mostraremos ningún dato de prueba, eso sería demasiado sencillo.
+"""
+
+# ingresa un valor flotante para la variable a aquí
+# ingresa un valor flotante para la variable b aquí
+
+# muestra el resultado de la suma aquí 
+# muestra el resultado de la resta aquí
+# muestra el resultado de la multiplicación aquí
+# muestra el resultado de la división aquí
+
+#print("\n¡Eso es todo, amigos!")
+
+variableA = float(input("Inserta el valor para la variable a: "))
+variableB = float(input("Inserta el valor para la variable b: "))
+
+print("La suma de la variable a y la variable b es: " + str(variableA + variableB))
+print("La resta de la variable a y la variable b es: " + str(variableA - variableB))
+print("La multiplicación de la variable a y la variable b es: " + str(variableA * variableB))
+print("La división de la variable a y la variable b es: " + str(variableA / variableB))
+
+print("\n¡Eso es todo, amigos!")
